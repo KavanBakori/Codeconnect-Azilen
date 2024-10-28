4c370453243aac3a5b1d3e6b35510ec1a1542b47
KavanBakori <kavanbakori111@gmail.com>
2024-10-28 19:37:41 +0530

    final

--- server/index.js
+++ server/index.js
@@ -8,24 +8,24 @@ const axios = require("axios");
 const server = http.createServer(app);
 require("dotenv").config();
 
-const languageConfig = {
-  python3: { versionIndex: "3" },
-  java: { versionIndex: "3" },
-  cpp: { versionIndex: "4" },
-  nodejs: { versionIndex: "3" },
-  c: { versionIndex: "4" },
-  ruby: { versionIndex: "3" },
-  go: { versionIndex: "3" },
-  scala: { versionIndex: "3" },
-  bash: { versionIndex: "3" },
-  sql: { versionIndex: "3" },
-  pascal: { versionIndex: "2" },
-  csharp: { versionIndex: "3" },
-  php: { versionIndex: "3" },
-  swift: { versionIndex: "3" },
-  rust: { versionIndex: "3" },
-  r: { versionIndex: "3" },
-};
+// const languageConfig = {
+//   python3: { versionIndex: "3" },
+//   java: { versionIndex: "3" },
+//   cpp: { versionIndex: "4" },
+//   nodejs: { versionIndex: "3" },
+//   c: { versionIndex: "4" },
+//   ruby: { versionIndex: "3" },
+//   go: { versionIndex: "3" },
+//   scala: { versionIndex: "3" },
+//   bash: { versionIndex: "3" },
+//   sql: { versionIndex: "3" },
+//   pascal: { versionIndex: "2" },
+//   csharp: { versionIndex: "3" },
+//   php: { versionIndex: "3" },
+//   swift: { versionIndex: "3" },
+//   rust: { versionIndex: "3" },
+//   r: { versionIndex: "3" },
+// };
 
 // Enable CORS
 app.use(cors());
@@ -93,24 +93,24 @@ io.on("connection", (socket) => {
   });
 });
 
-app.post("/compile", async (req, res) => {
-  const { code, language } = req.body;
+// app.post("/compile", async (req, res) => {
+//   const { code, language } = req.body;
 
-  try {
-    const response = await axios.post("https://api.jdoodle.com/v1/execute", {
-      script: code,
-      language: language,
-      versionIndex: languageConfig[language].versionIndex,
-      clientId: process.env.jDoodle_clientId,
-      clientSecret: process.env.kDoodle_clientSecret,
-    });
+//   try {
+//     const response = await axios.post("https://api.jdoodle.com/v1/execute", {
+//       script: code,
+//       language: language,
+//       versionIndex: languageConfig[language].versionIndex,
+//       clientId: process.env.jDoodle_clientId,
+//       clientSecret: process.env.kDoodle_clientSecret,
+//     });
 
-    res.json(response.data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Failed to compile code" });
-  }
-});
+//     res.json(response.data);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: "Failed to compile code" });
+//   }
+// });
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server is runnint on port ${PORT}`));
